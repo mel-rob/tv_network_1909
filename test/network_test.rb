de32449af7829f5b647113e022b32a0512fc415c
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-16 10:35:39 -0600

    Add Network test file

---
+++ test/network_test.rb
@@ -0,0 +1,56 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest < Minitest::Test
+
+  def setup
+    @nbc = Network.new("NBC")
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+  end
+
+  def test_it_exists
+    assert_instance_of Network, @nbc
+  end
+
+  def test_it_initializes_with_name_and_empty_array_of_shows
+    assert_equal "NBC", @nbc.name
+    assert_equal [], @nbc.shows
+  end
+
+  def test_it_can_add_shows
+    @nbc.add_show(@knight_rider)
+    assert_equal [@knight_rider], @nbc.shows
+    @nbc.add_show(@parks_and_rec)
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
+
+  def test_it_can_return_highest_paid_actor
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+    assert_equal "Amy Poehler", @nbc.highest_paid_actor
+  end
+
+  def test_it_can_return_payroll_hash
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    expected_hash = {
+                      "David Hasselhoff" => 1600000,
+                      "William Daniels" => 1000000,
+                      "Amy Poehler" => 2000000,
+                      "Nick Offerman" => 1400000
+                    }
+    assert_equal expected_hash, @nbc.payroll
+  end
+
+end
